c35e256ed04204cbbe27becdd34bf829d6e9ace9
Mehul Sachdeva <mehulsachdeva@Mehuls-MacBook-Pro.local>
2024-08-09 14:12:24 +0530

    Add basic dashboard page

--- src/components/containers/Dashboard/index.tsx
+++ src/components/containers/Dashboard/index.tsx
@@ -1,7 +1,17 @@
 import React from "react"
+import styles from "./index.module.css"
 
 const Dashboard = () => {
-	return <div>Dashboard</div>
+	return (
+		<div className={styles.container}>
+			<div className={styles.header}>
+				<div className={styles.title}>Hello Jane</div>
+				<div className={styles.subTitle}>
+					How are you doing today? Would you like to share something with the community 🤗
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Dashboard
