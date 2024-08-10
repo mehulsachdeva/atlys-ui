c4244d9c3d2eca71652fa3c22b6c76c0e94e9ea5
Mehul Sachdeva <mehulsachdeva@Mehuls-MacBook-Pro.local>
2024-08-10 19:59:07 +0530

    Minor changes

--- src/components/common/core/LoginModal/index.test.tsx
+++ src/components/common/core/LoginModal/index.test.tsx
@@ -1,24 +1,12 @@
 import "@testing-library/jest-dom"
 import { render, screen } from "@testing-library/react"
 import LoginModal from "."
-import { PORTAL_ID } from "constants/index"
 
 describe("Login Modal", () => {
 	const props = {
 		isOpen: true,
 		onClose: jest.fn(),
 	}
-
-	beforeEach(() => {
-		const portalRoot = document.createElement("div")
-		portalRoot.id = PORTAL_ID
-		document.body.appendChild(portalRoot)
-	})
-
-	afterEach(() => {
-		const portalRoot = document.getElementById(PORTAL_ID)
-		portalRoot && document.body.removeChild(portalRoot)
-	})
 
 	test("is not rendered when isOpen is false", () => {
 		render(<LoginModal {...props} isOpen={false} />)
